b32acf84fb626508b427f448decbd7d2e4c71f23
Terra-01 <thisisshivam@icloud.com>
2025-06-25 08:59:35 +0530

    removed markdown.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
       <main className="mx-auto max-w-screen-xl px-6 pt-8 pb-12 md:px-12 md:pt-16 md:pb-20">
         <header className="flex flex-col items-center gap-8 lg:flex-row lg:justify-between lg:items-start mb-8">
             <div className="flex items-center gap-4">
-                <Image src="/profile.jpg" alt="Profile Picture" width={80} height={80} className="rounded-full object-cover"/>
+                <Image src="/profile.jpg" alt="Profile Picture" width={80} height={80} className="w-20 h-20 rounded-full object-cover"/>
                 <div>
                     <h1 className="text-3xl font-bold tracking-tight text-slate-200">Your Name</h1>
                     <h2 className="mt-1 text-md font-medium tracking-tight text-slate-400">Your Title</h2>
